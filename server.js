f4e91c9e1c7eae85db74e8c518e2978ff706babf
Lynn Mitchell <lynn7mitchell@gmail.com>
2019-09-19 15:28:51 -0500

    heroku build

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ require("./routes/api/notes")(app)
 if (process.env.NODE_ENV === 'production') {
   // set static folder
   app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // app.get('*', (req, res) => {
+  //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
 }
 
 // Start the server
